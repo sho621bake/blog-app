d247cb417345fa23b4521c6082e6e5ee2b27caa7
sho621bake <sho6217@gmail.com>
2022-08-12 15:22:26 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Header from '../components/header'
+import Hero from '../components/hero'
+import Footer from '../components/footer'
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <main>
+                <Hero />
+            </main>
+            <Footer />
+        </>
+    )
+}
